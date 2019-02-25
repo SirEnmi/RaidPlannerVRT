5cc4630e259d718bd39bfa2fcf5f58a5c38c2953
Emilien <emilien.meissonnier@gmail.com>
2019-02-26 00:04:30 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -4,50 +4,59 @@
 	include "ressources/includes/connexion.php";
     include 'ressources/includes/header.php';
     include 'ressources/includes/navbar.php';
+
+    $rqPerso = "SELECT id_lodestone, job FROM personnages WHERE id_user = :id_user";
+    //preparation
+    $stmtPerso = $dbh->prepare($rqPerso);
+    //params
+    $params = array(':id_user' => $_SESSION['id']);
+    //execution
+    $stmtPerso->execute($params);
+    //recup données
+    $Perso = $stmtPerso->fetch();
 ?>
+
+<script>
+    $(document).ready(function () {
+        var job = "<?= $Perso['job'] ?>";
+        
+        $('option').each(function () {
+            if ($(this).attr("value") == job){
+                $(this).attr("selected","selected");
+                console.log('salut');
+            }
+        });
+    });
+</script>
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-dark">
-                <div class="card-header dark" style="color: white ">Modifier profil</div>
+                <div class="card-header dark" style="color: whitesmoke">Modifier profil</div>
                 <div class="card-body bg-secondary ">
-                    @isset($profil)
-                    <!--                        <form method="PATCH" action="{{ route('profil.update', $profil[0]->id) }}">-->
-                    <form method="POST" action="{{ route('profil.update', ['id'=>$profil[0]->id]) }}">
-                        <input type="hidden" name="_method" value="PATCH" /> {{csrf_field()}}
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Nom</label>
+                    <form method="POST" action="updateProfil.php">
+                        <!--<input type="hidden" name="_method" value="PATCH" /> {{csrf_field()}}-->
+                        <!--<div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Nom</label>
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{$profil[0]->name}}" required autofocus> @if ($errors->has('name'))
-                                <span class="invalid-feedback">
-
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span> @endif
+                                <input id="name" type="text" class="form-control" name="name" value="<?= $_SESSION['name'] ?>" required autofocus>
+                            </div>
+                        </div>-->
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Adresse Email</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="<?= $_SESSION['email'] ?>" required>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Adresse Email :</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Nouveau mot de passe</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{$profil[0]->email}}" required> @if ($errors->has('email'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span> @endif
+                                <input id="password" type="password" class="form-control" name="password">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"> @if ($errors->has('password'))
-                                <span class="invalid-feedback">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span> @endif
-                            </div>
-
-                        </div>
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmer à nouveau') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Confirmer à nouveau</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
@@ -55,35 +64,57 @@
 
                         </div>
                         <div class="form-group row">
-                            <label for="id_lodestone" class="col-md-4 col-form-label text-md-right">Id Personnage</label>
+                            <label for="id_lodestone" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Id Personnage</label>
 
                             <div class="col-md-6">
-                                <input id="id_lodestone" type="text" class="form-control" name="id_lodestone" value="{{$profil[0]->id_lodestone}}" required>
+                                <input id="id_lodestone" type="text" class="form-control" name="id_lodestone" value="<?= $Perso['id_lodestone'] ?>" required>
                             </div>
 
                         </div>
                         <div class="form-group row">
-                            <label for="job" class="col-md-4 col-form-label text-md-right">Job</label>
-
+                            <label for="job" class="col-md-4 col-form-label text-md-right" style="color: whitesmoke">Job</label>
                             <div class="col-md-6">
-                                <input id="job" type="text" class="form-control" name="job" value="{{$profil[0]->job}}">
+                                <select class="form-control" name="job" id="job">
+                                    <optgroup label="Tanks">
+                                        <option value="PLD">Paladin</option>
+                                        <option value="WAR">Guerrier</option>
+                                        <option value="DRK">Chevalier Noir</option>
+                                    </optgroup>
+                                    <optgroup label="Healers">
+                                        <option value="WHM">Mage Blanc</option>
+                                        <option value="AST">Astromancien</option>
+                                        <option value="SCH">Erudit</option>
+                                    </optgroup>
+                                    <optgroup label="DPS - Melee">
+                                        <option value="DRG">Dragoon</option>
+                                        <option value="NIN">Ninja</option>
+                                        <option value="MNK">Moine</option>
+                                        <option value="SAM">Samouraï</option>
+                                    </optgroup>
+                                    <optgroup label="DPS - Distance">
+                                        <option value="BRD">Bard</option>
+                                        <option value="MCH">Machiniste</option>
+                                        <option value="BLM">Mage Noir</option>
+                                        <option value="RDM">Mage Rouge</option>
+                                        <option value="SMN">Invocateur</option>
+                                    </optgroup>
+                                </select>
                             </div>
-
                         </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-white">
-                                    {{ __('Enregistrer') }}
+                                    Enregistrer
                                 </button>
                             </div>
                         </div>
                     </form>
-                    @endisset
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <?php
     include 'ressources/includes/footer.php';
 ?>
